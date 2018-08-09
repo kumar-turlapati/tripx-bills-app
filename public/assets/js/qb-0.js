45b18ef55bf65a35870a0a48513f46274a89cb7d
kumar-turlapati <kumar.turlapati@popcornapps.com>
2018-08-09 16:16:01 +0530

    javascript issue fixed.

--- public/assets/js/qb-0.js
+++ public/assets/js/qb-0.js
@@ -1,4 +1,5 @@
 $(window).load(function() {
+  $(".se-pre-con").fadeOut("slow");  
   setTimeout(function(){
     var bQi = new Fingerprint2();
     bQi.get(function(_bq_result) {
@@ -6,4 +7,82 @@ $(window).load(function() {
       $.post('/id__mapper', $('#bQ').serialize());
     });
   }, 100);
+  if( $('#sendOtpBtn').length>0 ) {
+    $('#sendOtpBtn').on('click', function(){
+      sendOTP();
+    });
+    $('#submit-fp').on('click', function(e){
+      var userId = $('#emailID').val();
+      var otp = $('#pass-fp').val();
+      var newPassword = $('#newpass-fp').val();
+      if(userId === '' || otp === '' || newPassword === '') {
+        alert('Userid, OTP and New password fields are mandatory to Reset your password.');
+        $('#emailID').focus();
+        return false;
+      }
+      /* hit server to reset the password */
+      jQuery.ajax("/reset-password", {
+        method:"POST",
+        data: $('#forgotPassword').serialize(),
+        success: function(response) {
+          if(response.status===false) {
+            alert(response.error);
+            window.location.href = '/forgot-password';
+          } else {
+            alert('Password has been changed successfully.');
+            window.location.href = '/login';
+          }
+        },
+        error: function(e) {
+          alert('Unable to reset password. Please try again.');
+          window.location.href = '/forgot-password';
+        }
+      });
+
+      e.preventDefault();
+    });
+  }
+  function sendOTP(fpType) {
+    var userId = $('#emailID').val();
+    var emailFilter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+    if(!emailFilter.test(userId)) {
+      $('#emailID').focus();
+      alert('Please enter a valid username.');
+      return false;
+    }
+
+    /* hit server to get the OTP */
+    jQuery.ajax("/send-otp", {
+      method:"POST",
+      data: $('#forgotPassword').serialize(),
+      success: function(response) {
+        if(response.status===false) {
+          alert(response.errortext);
+          return false;
+        }
+        if(response.status === true) {
+          $('#success-msg-fp').show();
+          $('#success-msg-fp').html(response.message);
+          $('#pass-fp').attr('disabled', false);
+          $('#submit-fp').attr('disabled', false);
+          $('#newpass-fp').attr('disabled', false);
+          $('#sendOtpBtn').attr('disabled', true);
+          if(fpType==='resend') {
+            alert('OTP has been resent successfully. Please use latest code to reset your password.');
+          }
+        } else {
+          $('#error-msg-fp').show();
+          $('#error-msg-fp').html(response.message);
+          if(fpType==='resend') {
+            alert('Unable to resend OTP.');
+          }        
+        }
+      },
+      error: function(e) {
+        $('#emailID').focus();
+        alert('An error occurred while processing your request.');
+        return false;
+      }
+    });
+  }
 });
