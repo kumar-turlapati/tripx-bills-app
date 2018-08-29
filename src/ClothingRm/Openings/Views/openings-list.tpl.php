6c41a831216378271f49fedd2cd9142c4ef18a6b
kumar-turlapati <kumar.turlapati@popcornapps.com>
2018-08-29 17:29:38 +0530

    table cleaned.

--- src/ClothingRm/Openings/Views/openings-list.tpl.php
+++ src/ClothingRm/Openings/Views/openings-list.tpl.php
@@ -111,6 +111,8 @@
                 <th width="25%" class="text-left valign-middle">Item name</th>
                 <th width="10%" class="text-left valign-middle">Category</th> 
                 <th width="5%" class="text-center valign-middle">Opening qty.</th>
+                <th width="5%" class="text-center valign-middle">Packed/Unit</th>
+                <th width="5%" class="text-center valign-middle">Total qty.</th>                
                 <th width="8%" class="text-center valign-middle">Opening rate<br />(in Rs.)</th>
                 <th width="8%" class="text-center valign-middle">Opening value<br />(in Rs.)</th>
                 <th width="8%" class="text-center valign-middle">Purchase rate<br />(in Rs.)</th>                
@@ -131,6 +133,7 @@
                   $tax_percent = $opening_details['taxPercent'];
                   $opening_code = $opening_details['openingCode'];
                   $packed_qty = $opening_details['packedQty'];
+                  $total_qty = round($opening_qty * $packed_qty, 2);
 
                   $opening_value = round($opening_qty*$opening_rate*$packed_qty,2);
 
@@ -141,6 +144,8 @@
                     <td class="text-left med-name valign-middle"><?php echo $item_name ?></td>
                     <td class="text-left med-name valign-middle"><?php echo $category_name ?></td>                    
                     <td class="valign-middle"><?php echo $opening_qty ?></td>
+                    <td class="valign-middle"><?php echo $packed_qty ?></td>
+                    <td class="valign-middle"><?php echo $total_qty ?></td>                    
                     <td class="valign-middle"><?php echo number_format($opening_rate,2,'.','') ?></td>
                     <td class="text-bold valign-middle"><?php echo number_format($opening_value,2,'.','') ?></td>
                     <td class="text-bold valign-middle"><?php echo number_format($purchase_rate,2,'.','') ?></td>                    
@@ -164,7 +169,7 @@
               endforeach; 
             ?>
               <tr>
-                <td colspan="5" align="right">PAGE TOTALS (PURCHASE VALUE)</td>
+                <td colspan="7" align="right">PAGE TOTALS (PURCHASE VALUE)</td>
                 <td align="right" style="font-weight:bold;font-size:14px;"><?php echo number_format($tot_opening_stock_value,2,'.','') ?></td>
                 <td>&nbsp;</td>
                 <td>&nbsp;</td>
